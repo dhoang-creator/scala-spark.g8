89ee78bd32f2e7134439befde6f47f7ebc16d906
Duy Hoang <duyhoang123@hotmail.co.uk>
2024-07-12 21:41:14 +0200

    docker compose commands taken out completely

--- src/main/g8/run.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-
-docker compose down -v
-docker compose up spark-worker -d
-docker compose up $name;format="normalize"$-spark-client
-docker compose down -v
--- src/main/g8/run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-docker compose down -v
-docker compose up spark-worker -d
-docker compose up $name;format="normalize"$-spark-client
-docker compose down -v
